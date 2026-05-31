a6ab26601e8e58d828ddac08ceeecb94a9707219
TrungDung <nguyentrungdung0809@gmail.com>
2026-05-31 15:42:50 +0700

    update

--- predict.py
+++ predict.py
@@ -1,37 +1,28 @@
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
 import numpy as np
 import os
+from keras.models import load_model
+from keras.utils import load_img, img_to_array
 
-model = load_model("cards_model.h5")
+model = load_model("cards_model.keras")
 
-class_names = sorted(
-    os.listdir("dataset/train")
-)
+class_names = sorted(os.listdir("dataset/train"))
 
 def predict_card(img_path):
-
-    img = image.load_img(
+    img = load_img(
         img_path,
-        target_size=(128,128)
+        target_size=(128, 128)
     )
-
-    img_array = image.img_to_array(img)
-
-    img_array = img_array / 255.0
-
+    img_array = img_to_array(img)
+    # img_array = img_array / 255.0
     img_array = np.expand_dims(
         img_array,
         axis=0
     )
-
     prediction = model.predict(img_array)
-
     class_index = np.argmax(prediction)
-
     confidence = np.max(prediction)
-
     return (
         class_names[class_index],
         confidence
     )
+
--- train.py
+++ train.py
@@ -1,46 +1,38 @@
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D
-from tensorflow.keras.layers import Flatten, Dense, Dropout
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras.utils import image_dataset_from_directory
+from keras.layers import Rescaling
 
 train_path = "dataset/train"
 valid_path = "dataset/valid"
 
-train_data = ImageDataGenerator(
-    rescale=1./255
-).flow_from_directory(
+train_data = image_dataset_from_directory(
     train_path,
-    target_size=(128,128),
+    image_size=(128, 128),
     batch_size=32,
-    class_mode="categorical"
+    label_mode="categorical"
 )
 
-valid_data = ImageDataGenerator(
-    rescale=1./255
-).flow_from_directory(
+valid_data = image_dataset_from_directory(
     valid_path,
-    target_size=(128,128),
+    image_size=(128, 128),
     batch_size=32,
-    class_mode="categorical"
+    label_mode="categorical"
 )
 
 model = Sequential([
-    Conv2D(32,(3,3),activation="relu",input_shape=(128,128,3)),
-    MaxPooling2D(2,2),
-
-    Conv2D(64,(3,3),activation="relu"),
-    MaxPooling2D(2,2),
-
-    Conv2D(128,(3,3),activation="relu"),
-    MaxPooling2D(2,2),
-
+    Rescaling(1./255, input_shape=(128, 128, 3)), 
+    Conv2D(32, (3, 3), activation="relu"),
+    MaxPooling2D(2, 2),
+    Conv2D(64, (3, 3), activation="relu"),
+    MaxPooling2D(2, 2),
+    Conv2D(128, (3, 3), activation="relu"),
+    MaxPooling2D(2, 2),
     Flatten(),
-
-    Dense(128,activation="relu"),
+    Dense(128, activation="relu"),
     Dropout(0.5),
-
-    Dense(53,activation="softmax")
+    Dense(53, activation="softmax")
 ])
 
 model.compile(
